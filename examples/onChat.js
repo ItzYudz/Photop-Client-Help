2e67010824b16fa9009a21e9fd35f8b626d15bae
qaa <95725567+ItzYudz@users.noreply.github.com>
2022-06-25 19:51:49 -0700

    Update onChat.js

--- examples/onChat.js
+++ examples/onChat.js
@@ -1,1 +1,7 @@
-
+const { Client } = require('photop-client')
+const client = new Client({ username: 'bot username', password: process.env['Pass'] })
+client.onPost = (post) => {
+  if (post.text == "test") {
+    post.chat("I'm working")
+  }
+}
